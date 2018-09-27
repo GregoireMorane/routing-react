b9ab1dba7c3b8ce3c68608d80d792112e8da881b
GregoireMorane <gh.morane@gmail.com>
2018-09-27 14:18:27 +0200

    Finalisation routes

--- src/History.js
+++ src/History.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 
 class History extends Component {
     render() {
         return (
             <div>
                 <h1>Notre histoire</h1>
-                <NavLink to="/"> Accueil </NavLink>
-                <NavLink to="/notre-histoire" activeStyle={{fontWeight: 'bold',color: 'red'}}> Histoire </NavLink>
             </div>
         );
     }
